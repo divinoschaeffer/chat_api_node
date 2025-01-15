0a58ff722c6156397036dd9624259b8da2b4ff12
Divino Schaeffer <divino.schaeffer02@gmail.com>
2025-01-15 20:24:36 +0100

    🔨 Run tests from terminal

--- .docker/npm.sh
+++ .docker/npm.sh
@@ -5,13 +5,13 @@ CONTAINER_NAME="chat-api"
 if [[ $# -ne 1 ]]; then
   echo "Usage: $0 <npm_command>"
   echo "Example: $0 install"
-  echo "Commands: install | update"
+  echo "Commands: install | update | test"
   exit 1
 fi
 
 NPM_COMMAND=$1
 
-if [[ "$NPM_COMMAND" != "install" && "$NPM_COMMAND" != "update" ]]; then
+if [[ "$NPM_COMMAND" != "install" && "$NPM_COMMAND" != "update" && "$NPM_COMMAND" != "test" ]]; then
   echo "Error: Invalid command '$NPM_COMMAND'. Use 'install' or 'update'."
   exit 1
 fi
