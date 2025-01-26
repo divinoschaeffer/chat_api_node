67b3dc532a9a5d49a442a18b8ddf1a0d233ecc2e
Divino Schaeffer <divino.schaeffer02@gmail.com>
2025-01-26 19:31:21 +0100

    🚧 Count, list , softDelete and getById Message

--- src/repositories/messageRepository.ts
+++ src/repositories/messageRepository.ts
@@ -1,5 +1,6 @@
 import {Message} from "../models/Message";
 import database from "../database";
+import {messagesFromDbList} from "./factories/messageFactory";
 
 export const store = async (message: Message): Promise<Message> => {
     return await database.transaction(async (trx) => {
@@ -27,6 +28,56 @@ export const store = async (message: Message): Promise<Message> => {
             throw e;
         }
     })
+}
+
+export const count = async (chatId: number): Promise<number> => {
+   const [result] = await database('messages')
+       .where('chat_id', chatId)
+       .where('deleted_at', null)
+       .count({ count: '*' });
+
+   if (result.count === undefined)
+   {
+       return 0;
+   }
+
+   return result.count as number;
+}
+
+export const list = async (chatId: number, limit: number, offset: number): Promise<Message[]> => {
+    const result = await database<Message>('messages')
+        .select([
+            ...getMessageColumns(),
+            'images_messages.image_path as images_messages_image_path'
+        ])
+        .leftJoin('images_messages', 'images_messages.message_id', '=', 'messages.id')
+        .offset(offset)
+        .limit(limit)
+        .where('chat_id', chatId)
+        .where('deleted_at', null)
+        .orderBy('created_at', 'desc');
+
+    return messagesFromDbList(result);
+}
+
+export const softDelete = async (messageId: number): Promise<void> => {
+    await database<Message>('messages')
+        .update('deleted_at', new Date())
+        .where('id', messageId);
+}
+
+export const getMessageById = async (messageId: number): Promise<Message|null> => {
+    const result = await database<Message>('messages')
+        .select([
+            ...getMessageColumns(),
+            'images_messages.image_path as images_messages_image_path'
+        ])
+        .leftJoin('images_messages', 'images_messages.message_id', '=', 'messages.id')
+        .where('message_id', messageId)
+        .where('deleted_at', null);
+
+    const list: Message[] = messagesFromDbList(result);
+    return list.length === 0 ? null : list[0];
 }
 
 export const getMessageColumns = (): string[] => {
