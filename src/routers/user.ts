20a9fb3ae14682bfc9b0ed4f0f0a82ad9ffb282c
Divino Schaeffer <divino.schaeffer02@gmail.com>
2025-01-28 19:54:40 +0100

    📝 Get user by name doc

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -3,6 +3,36 @@ import {getUserController} from "../features/user/get/getUserController";
 
 const userRouter: Router = Router();
 
+/**
+ * @swagger
+ * /api/user/{user_name}:
+ *   get:
+ *     summary: Retrieve user details by username
+ *     description: Fetches details of a user based on the provided username.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: user_name
+ *         required: true
+ *         description: The username of the user to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: User details successfully retrieved.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  user:
+ *                      $ref: '#/components/schemas/PublicUserDTO'
+ *       404:
+ *         description: User not found.
+ *       500:
+ *         description: Server error.
+ */
 userRouter.get('/:user_name', getUserController);
 
 export default userRouter;
