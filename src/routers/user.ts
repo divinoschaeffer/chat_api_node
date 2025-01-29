7bc38ace9df5b373927e870bab19a9a2b547bab4
Divino Schaeffer <divino.schaeffer02@gmail.com>
2025-01-29 20:11:41 +0100

    📝 Get all user chats doc

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -36,6 +36,36 @@ const userRouter: Router = Router();
  */
 userRouter.get('/:user_name', getUserController);
 
+/**
+ * @swagger
+ * /api/user/{user_id}/chat:
+ *   get:
+ *     summary: Retrieve user chats
+ *     description: Fetches all chats associated with a specific user.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: user_id
+ *         required: true
+ *         description: The ID of the user whose chats should be retrieved.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of user chats successfully retrieved.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Chat'
+ *       404:
+ *         description: User not found or no chats available.
+ *       500:
+ *         description: Server error.
+ */
 userRouter.get('/:user_id/chat', getUserChatsController);
+
 
 export default userRouter;
